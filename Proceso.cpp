3e1aa47210fb3ccfc61286ef37fa8dc1dd43ae44
Alberto <148889956+AlbeTroll@users.noreply.github.com>
2024-10-15 09:00:36 +0200

    Update: getters Proceso.cpp

--- Proceso.cpp
+++ Proceso.cpp
@@ -1,9 +1,28 @@
 #include "Proceso.h"
 
 Proceso::Proceso(int PID, int PPID=1, int priority, int core) {
-
 }
 
 Proceso::~Proceso() {
-
 }
+
+int Proceso::getPID(){
+  return PID;
+  }
+
+int Proceso::getPPID(){
+  return PPID;
+  }
+
+int Proceso::getPriority(){
+  return priority;
+  }
+
+int Proceso::getCore(){
+  return core;
+  }
+
+int Proceso::getStartTime(){
+  return startTime;
+  }
+
