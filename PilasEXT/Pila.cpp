174ab82104c88bcfa5db4c127c25cf90b8f18ac1
Alberto <148889956+AlbeTroll@users.noreply.github.com>
2024-10-15 09:28:17 +0200

    Update: ejercicios 1 y 2 hechos

--- PilasEXT/Pila.cpp
+++ PilasEXT/Pila.cpp
@@ -5,156 +5,158 @@ using namespace std;
 
 // Constructor para la pila
 Pila::Pila() {
-	cima = NULL;
+    cima = NULL;
 }
 
 // Destructor
 Pila::~Pila() {
-	while(cima) pop();
+    while (cima) pop();
 }
 
 // Push item to the top
 void Pila::push(int n) {
-	pnodo nuevo = new Nodo(n, cima);
-	cima = nuevo;
+    pnodo nuevo = new Nodo(n, cima);
+    cima = nuevo;
 }
 
 // Pop the last item (LIFO)
 void Pila::pop() {
-	pnodo nodo;
+    pnodo nodo;
 
-	if(cima) {
-		nodo = cima;
-		cima = nodo->next;
-		delete nodo;
-	}
+    if (cima) {
+        nodo = cima;
+        cima = nodo->next;
+        delete nodo;
+    }
 }
 
 // Check if stack is empty
 bool Pila::isEmpty() {
-	return cima == NULL;
+    return cima == NULL;
 }
 
 // Length of the stack
 int Pila::length() {
-	Pila aux;
-	int length = 0;
+    Pila aux;
+    int length = 0;
 
-	while(!isEmpty()) {
-		aux.push(top());
-		pop();
-		length++;
-	}
+    while (!isEmpty()) {
+        aux.push(top());
+        pop();
+        length++;
+    }
 
-	for(int i = 0; i<length; i++) {
-		push(aux.cima->value);
-		aux.pop();
-	}
+    for (int i = 0; i < length; i++) {
+        push(aux.cima->value);
+        aux.pop();
+    }
 
-	return length;
+    return length;
 }
 
 // View the top (without popping)
 int Pila::top() {
-	if(!isEmpty()) {
-		return cima->value;
-	} else {
-		return -0;
-	}
+    if (!isEmpty()) {
+        return cima->value;
+    } else {
+        return -0;
+    }
 }
 
 // View the bottom
 int Pila::last() {
-	reverse();
-	int n = cima->value;
-	reverse();
+    reverse();
+    int n = cima->value;
+    reverse();
 
-	return n;	
+    return n;
 }
 
 // Reverse the stack
 void Pila::reverse() {
-	Pila aux;
-	Pila aux2;
+    Pila aux;
+    Pila aux2;
 
-	while(!isEmpty()) {
-		aux.push(cima->value);
-		pop();
-	}
+    while (!isEmpty()) {
+        aux.push(cima->value);
+        pop();
+    }
 
-	while(!aux.isEmpty()) {
-		aux2.push(aux.cima->value);
-		aux.pop();
-	}
+    while (!aux.isEmpty()) {
+        aux2.push(aux.cima->value);
+        aux.pop();
+    }
 
-	while(!aux2.isEmpty()) {
-		push(aux2.cima->value);
-		aux2.pop();
-	}
-
+    while (!aux2.isEmpty()) {
+        push(aux2.cima->value);
+        aux2.pop();
+    }
 }
 
 // Print the top item
 void Pila::toString() {
-	if(isEmpty()) {
-		cout << "La pila está vacía" << endl;		
-	} else {
-		cout << "Cima de la pila: " << cima->value << endl; 
-	}
+    if (isEmpty()) {
+        cout << "La pila está vacía" << endl;
+    } else {
+        cout << "Cima de la pila: " << cima->value << endl;
+    }
 }
 
 // Print all items
 void Pila::showAll() {
-	Pila tmp;
+    Pila tmp;
 
-	while(!isEmpty()) {
-		cout << cima->value << ", ";
-		tmp.push(cima->value);
-		pop();
-	}
+    while (!isEmpty()) {
+        cout << cima->value << ", ";
+        tmp.push(cima->value);
+        pop();
+    }
 
-	cout << "\n";
+    cout << "\n";
 
-	while(!tmp.isEmpty()) {
-		push(tmp.cima->value);
-		tmp.pop();
-	}
+    while (!tmp.isEmpty()) {
+        push(tmp.cima->value);
+        tmp.pop();
+    }
 }
 
 // Pop the last item
 void Pila::popLast() {
-	reverse();
-	pop();
-	reverse();
+    reverse();
+    pop();
+    reverse();
 }
 
 // Pop n first items
 void Pila::popRange(int n) {
-	for(int i = 0; i<n; i++) pop();
+    for (int i = 0; i < n; i++) pop();
 }
 
 // Add sec to the stack
 void Pila::concat(Pila sec) {
-	sec.reverse();
+    sec.reverse();
 
-	while(!sec.isEmpty()) {
-		push(sec.cima->value);
-		sec.cima = sec.cima->next;
-	}
+    while (!sec.isEmpty()) {
+        push(sec.cima->value);
+        sec.cima = sec.cima->next;
+    }
 }
 
 // Check is stack is ordered
 bool Pila::isOrder() {
-	bool order = true;
-	pnodo aux = cima;
+    bool order = true;
+    pnodo aux = cima;
 
-	while(aux->next != NULL && order) {
-		if(aux->value > aux->next->value) {
-			order = false;
-		}
+    while (aux->next != NULL && order) {
+        if (aux->value > aux->next->value) {
+            order = false;
+        }
 
-		aux = aux->next;
-	}
+        aux = aux->next;
+    }
 
-	return order;
+    return order;
 }
+
+// Remove all items
+//TODO: void Pila::removeAll() { para asignar cada proceso a su cola, cada vez que hagamos pop.
--- main.cpp
+++ main.cpp
@@ -15,15 +15,39 @@ using namespace std;
 
 
 int main() {
+	Pila procesos;
+	Cola nucleo1;
+	Cola nucleo2;
+	Cola nucleo3;
+	Cola colaEspera;
 
 	//1) Crear la pila de procesos, los procesos los creamos manualmente.
+	Proceso p1(1, 69, 0, 1);
+	Proceso p2(2, 33, 9, 2);
+	Proceso p3(3, 214, 2, 3);
+	Proceso p4(4, 352, 7, 1);
+	Proceso p5(5, 122, 3, 2);
+	Proceso p6(6, 11, 2, 3);
+	Proceso p7(7, 649, 3, 1);
+	Proceso p8(8, 324, 5, 2);
+	Proceso p9(9, 342, 7, 3);
+	Proceso p10(10, 786, 2, 1);
 
+	procesos.push(p1);
+	procesos.push(p2);
+	procesos.push(p3);
+	procesos.push(p4);
+	procesos.push(p5);
+	procesos.push(p6);
+	procesos.push(p7);
+	procesos.push(p8);
+	procesos.push(p9);
+	procesos.push(p10);
 
 	//2) Mostrar todos los procesos de la pila (podemos utilizar el método showAll).
-
+	procesos.showAll();
 
 	//3) Borrar la pila de procesos
-
 
 	//4) Mostrar la cola de espera de procesos.
 
